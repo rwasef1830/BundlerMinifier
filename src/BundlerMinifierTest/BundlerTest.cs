9d59ac738623ad7c410c40054357788e428307b4
Raif Atef <beliskner.github.3psil0n@neverbox.com>
2024-06-21 22:50:13 +0300

    Fix failing test.

--- src/BundlerMinifierTest/BundlerTest.cs
+++ src/BundlerMinifierTest/BundlerTest.cs
@@ -180,7 +180,7 @@ public class BundlerTest
         Assert.IsFalse(File.Exists("../../../artifacts/foo.min.js"));
         Assert.IsTrue(File.Exists("../../../artifacts/foo.js.gz"));
         Assert.IsTrue(File.Exists("../../../artifacts/foo.js.br"));
-        Assert.IsTrue(File.Exists("../../../artifacts/foo.js.zst"));
+        Assert.IsTrue(File.Exists("../../../artifacts/foo.js.zstd"));
         Assert.IsTrue(File.Exists("../../../artifacts/minify.min.js"));
         Assert.IsTrue(File.Exists("../../../artifacts/minify.min.js.gz"));
         Assert.IsTrue(File.Exists("../../../artifacts/minify.min.js.zstd"));
